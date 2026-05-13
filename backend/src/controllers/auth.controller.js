2b8ac70e62c82047b0588a6f9c6030dc990a97e2
Pooja Jat <patelpoojajat@gmail.com>
2026-05-13 17:34:45 +0530

    added new features

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,12 +31,11 @@ const register = async (req, res) => {
       });
     }
 
-    // Create Normal User
     const user = await User.create({
       name,
       email,
       password,
-      role: "user",
+      role: "admin",
     });
 
     // Generate Token
